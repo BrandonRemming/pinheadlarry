2dd6cda59fea53397de964b6fe91825aab2e5cad
PinheadLarryDev <69769058+PinheadLarryDev@users.noreply.github.com>
2020-08-22 14:52:12 -0500

    Update celsiustofahrenheit.js

--- commands/celsiustofahrenheit.js
+++ commands/celsiustofahrenheit.js
@@ -5,13 +5,15 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
   const notNumberResponse = "you need to provide a temperature value. Derp."
   const tooLowResponse = "you need to provide a temperature value greater than -273.15°C. Derp.";
   const isNumber = val => typeof val === 'number' && val === val;
+  const toDecimalMark = num => num.toLocaleString('en-US');
 
-  var celsius = parseFloat(args[0])
-  if (isNumber(celsius) === false) return message.reply(notNumberResponse);
-  if (celsius < -273.15) return message.reply(tooLowResponse);
+  var arguments = parseFloat(args[0]);
+  if (isNumber(arguments) === false) return message.reply(notNumberResponse);
+  if (arguments < -273.15) return message.reply(tooLowResponse);
 
   const celsiusToFahrenheit = degrees => 1.8 * degrees + 32;
-  var fahrenheit = celsiusToFahrenheit(celsius);
+  var fahrenheit = toDecimalMark(celsiusToFahrenheit(arguments));
+  var celsius = toDecimalMark(arguments);
 
   const celsiusToFahrenheitEmbed = new Discord.RichEmbed()
     .setColor([255,0,0])
